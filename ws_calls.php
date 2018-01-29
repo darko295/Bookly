760ee09aff32554f3bdb6f63c869c69c167725c2
darko295 <darkob295@gmail.com>
2018-01-29 20:50:02 +0100

    Web service added

---
+++ ws_calls.php
@@ -0,0 +1,36 @@
+<?php
+include "connection.php";
+
+    function add_to_wishlist($user_id, $author_id, $book_id){
+        global $mysqli;
+        $sql = "INSERT INTO wishlist (userID, authorID, bookID) VALUES ('" . $user_id . "','" . $author_id . "','" . $book_id . "')";
+
+        if ($mysqli->query($sql)) {
+            echo "1";
+        } else {
+            echo "0";
+        }
+    }
+
+
+if (isset ($_POST["action"]) && isset($_POST["user_id"]) && isset($_POST["author_id"]) && isset($_POST['book_id'])){
+
+    $user_id = (int) $_POST['user_id'];
+    $author_id = (int) $_POST['author_id'];
+    $book_id = (int) $_POST['book_id'];
+
+    switch ($_POST["action"]){
+
+        case "wishlist" :
+            add_to_wishlist($user_id, $author_id, $book_id);
+            break;
+
+    }
+
+
+
+
+}else{
+    echo "<script> alert('Nisu GETavljeni svi parametri')</script>";
+
+}
